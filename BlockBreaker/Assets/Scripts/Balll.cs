fbb8636184e822308de5b3fba7d6e3b219f83dc6
william novaes <williamf.novaes@gmail.com>
2020-01-24 00:02:11 -0300

    novo projeto block breaker, base, ball, paredes, bouncy

---
+++ BlockBreaker/Assets/Scripts/Balll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balll : MonoBehaviour
+{
+    private Rigidbody2D ballRb;
+    [SerializeField] private float force;
+    [SerializeField] private LayerMask whatIsGround;
+    private LayerMask ground;
+    void Awake() {
+        ballRb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRb.AddForce(new Vector2(force, force));
+        ground = LayerMask.GetMask("Ground");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject != null && other.gameObject.tag.Equals("Ground")) {
+            Debug.Log("Morreu");
+        }
+    }
+}
---
+++ BlockBreaker/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    private Rigidbody2D playerRb;
+
+    void Start () {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        playerRb.velocity = new Vector2(horizontal * velocity, playerRb.velocity.y);
+    }
+}
